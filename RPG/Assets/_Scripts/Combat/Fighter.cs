d268f0eed820e6715925a52024ce45441d6414f1
edenizuygun <esma.deniz.uygun@gmail.com>
2023-06-19 13:49:31 +0300

    Hit Animations

--- RPG/Assets/_Scripts/Combat/Fighter.cs
+++ RPG/Assets/_Scripts/Combat/Fighter.cs
@@ -7,9 +7,12 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange=2f;
+        [SerializeField] float timeBetweenAttacks=1f;
+        float timeSinceLastAttack = 0f;
         Transform target;
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
             if (target == null) return;
 
             if (!GetsInRange())
@@ -19,12 +22,23 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
-            
-           
+
+
 
 
         }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack >timeBetweenAttacks)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0f;
+            }
+        }
+
         private bool GetsInRange() 
         {
             return Vector3.Distance(transform.position, target.position) < weaponRange;
@@ -32,12 +46,14 @@ namespace RPG.Combat
         public void Attack(CombatTarget combatTarget) 
         {
             GetComponent<ActionScheduler>().StartAction(this);
+            
             target = combatTarget.transform;
             Debug.Log("hiya");
         }
         public void Cancel() { target = null; }
 
-        
+        //AnimationEvent
+        void Hit() { }
     }
 }
 
